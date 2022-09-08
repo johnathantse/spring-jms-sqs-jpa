7220b5e7ba1eb10d096b97429bb166d385d6cbb7
Johnathan <johnathantse@LAPTOP-0VOPESAG.localdomain>
2022-09-07 20:30:30 -0700

    Create spring boot projeect

---
+++ src/main/java/com/jms/aws/springbootjmsaws/SpringbootJmsAwsApplication.java
@@ -0,0 +1,13 @@
+package com.jms.aws.springbootjmsaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJmsAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJmsAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jms/aws/springbootjmsaws/SpringbootJmsAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jms.aws.springbootjmsaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJmsAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
